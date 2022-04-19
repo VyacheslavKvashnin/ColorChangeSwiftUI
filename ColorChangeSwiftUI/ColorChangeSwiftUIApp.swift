6939b8c638786cb1d155486b639f8af2f2a16b4e
komplektan <54449303+komplektan@users.noreply.github.com>
2022-04-19 13:32:07 +0500

    Initial Commit

---
+++ ColorChangeSwiftUI/ColorChangeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorChangeSwiftUIApp.swift
+//  ColorChangeSwiftUI
+//
+//  Created by Вячеслав Квашнин on 19.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorChangeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorChangeSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ColorChangeSwiftUI
+//
+//  Created by Вячеслав Квашнин on 19.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
